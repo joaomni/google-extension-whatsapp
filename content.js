354e263e8d1599c5c17eb3db5935f28b409e1c9e
joaoqsl <joaojpmoreira25@gmail.com>
2020-04-16 22:59:50 -0300

    Add project files

---
+++ content.js
@@ -0,0 +1,26 @@
+const interval = setInterval(() => {
+    const header = document.querySelector("._3auIg")
+
+    if(header){
+        clearInterval(interval)
+
+
+        const button = document.createElement("button")
+
+        button.innerHTML = "2x"
+
+        button.classList.add("twoTimeButton")
+
+        button.addEventListener("click", () => {
+            const audios = document.querySelectorAll("audio")
+            audios.forEach((audio) => {
+                audio.playbackRate = 2;
+            })
+        })
+        
+        header.appendChild(button)
+
+
+    }
+}, 1000)
+
